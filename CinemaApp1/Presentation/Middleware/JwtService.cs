02fa65c4ef0d20ff6e74992b9c4c73e3bf579ebd
Luka Stankovic <lukastankovic.sd36@gmail.com>
2026-05-07 16:33:16 +0200

    JWT

---
+++ CinemaApp1/Presentation/Middleware/JwtService.cs
@@ -0,0 +1,15 @@
+﻿using CinemaApp.Data;
+
+namespace CinemaApp1.Presentation.Middleware
+{
+    public class JwtService
+    {
+        private readonly AppDbContext _dbcontext;
+        private readonly IConfiguration _configuration;
+        public JwtService(AppDbContext dbContext, IConfiguration configuration)
+        {
+            _dbcontext = dbContext;
+            _configuration = configuration;
+        } 
+    }
+}
--- CinemaApp1/Presentation/Program.cs
+++ CinemaApp1/Presentation/Program.cs
@@ -1,5 +1,8 @@
 using CinemaApp.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,31 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"])),
+        ClockSkew = TimeSpan.Zero
+    };
+
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -24,6 +52,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
